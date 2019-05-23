0b03e1f43b64200491075e5bb9b5420b2e85543c
VladimirBrejcha <python2python@icloud.com>
2019-05-23 18:24:11 +0300

    Initial Commit

---
+++ Rise/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Rise
+//
+//  Created by Vladimir Korolev on 23/05/2019.
+//  Copyright © 2019 VladimirBrejcha. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
