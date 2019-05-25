62204b66bffea410badfe6c617df68038533aac7
VladimirBrejcha <python2python@icloud.com>
2019-05-25 19:21:27 +0300

    ui changes

--- Rise/Controller/PersonalTimeViewController.swift
+++ Rise/Controller/PersonalTimeViewController.swift
@@ -9,34 +9,21 @@
 import UIKit
 import DatePickerCell
 
-class PersonalTimeViewController: UITableViewController {
-    var cells = [[UIView]]()
+class PersonalTimeViewController: UITableViewController, DatePickerCellDelegate {
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        self.tableView = UITableView(frame: CGRect.zero, style: .grouped)
-        self.tableView.rowHeight = UITableView.automaticDimension
-        // Removes extra separators.
-        self.tableView.tableFooterView = UIView()
-        
-        // The DatePickerCell.
-        let datePickerCell = DatePickerCell(style: UITableViewCell.CellStyle.default, reuseIdentifier: nil)
-        // Cells is a 2D array containing sections and rows.
-        cells = [[datePickerCell]]
     }
-
-
-    
     override func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
         // Get the correct height if the cell is a DatePickerCell.
-        let cell = cells[indexPath.section][indexPath.row]
+        let cell = tableView.cellForRow(at: indexPath)
         if (cell is DatePickerCell) {
             return (cell as! DatePickerCell).datePickerHeight()
         }
         return super.tableView(tableView, heightForRowAt: indexPath)
     }
-    
+
     override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
         // Deselect automatically if the cell is a DatePickerCell.
         let cell = self.tableView.cellForRow(at: indexPath)
@@ -45,18 +32,6 @@ class PersonalTimeViewController: UITableViewController {
             datePickerTableViewCell.selectedInTableView(tableView)
             self.tableView.deselectRow(at: indexPath, animated: true)
         }
-    }
-    
-    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return cells[section].count
-    }
-    
-    override func numberOfSections(in tableView: UITableView) -> Int {
-        return cells.count
-    }
-    
-    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        return cells[indexPath.section][indexPath.row] as! UITableViewCell
     }
     
 }
