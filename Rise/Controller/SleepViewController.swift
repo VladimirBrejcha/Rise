6e980a36c0aef87222e640904700fd7886e33f02
VladimirBrejcha <python2python@icloud.com>
2019-06-02 21:06:45 +0300

    code refactoring

--- Rise/Controller/SleepViewController.swift
+++ Rise/Controller/SleepViewController.swift
@@ -21,11 +21,23 @@ class SleepViewController: UIViewController {
         super.viewDidLoad()
         setupTimePicker()
         registerLocal()
+        setupDodoBar()
     }
 
     // MARK: UI setup methods
     private func setupTimePicker() {
         timePicker.setValue(UIColor.white, forKeyPath: "textColor")
+    }
+    
+    private func setupDodoBar() {
+        view.dodo.style.bar.cornerRadius = 12
+        view.dodo.style.bar.hideAfterDelaySeconds = 3
+        view.dodo.style.bar.backgroundColor = #colorLiteral(red: 0.262835294, green: 0.8022480607, blue: 0.3886030316, alpha: 0.6978983275)
+        
+        view.dodo.style.bar.animationShow = DodoAnimations.fade.show
+        view.dodo.style.bar.animationHide = DodoAnimations.fade.hide
+        
+        view.dodo.topAnchor = view.safeAreaLayoutGuide.topAnchor
     }
 
     // MARK: Notification center methods
@@ -63,30 +75,33 @@ class SleepViewController: UIViewController {
         center.add(request)
 
     }
+    
+    //MARK: IBActions
     @IBAction func sleepButtonPressed(_ sender: UIButton) {
         scheduleLocal()
     }
     
     @IBAction func bottomButton(_ sender: UIButton) {
         let storyboard = UIStoryboard(name: "Main", bundle: nil)
+        
         let controller = storyboard.instantiateViewController(withIdentifier: "personalViewController")
+        
         let transitionDelegate = SPStorkTransitioningDelegate()
+        
         controller.transitioningDelegate = transitionDelegate
         controller.modalPresentationStyle = .custom
         controller.modalPresentationCapturesStatusBarAppearance = true
+        
         transitionDelegate.storkDelegate = self
+        
         self.present(controller, animated: true, completion: nil)
     }
     
 }
+
 extension SleepViewController: SPStorkControllerDelegate {
+    
     func didDismissStorkBySwipe() {
-        view.dodo.style.bar.cornerRadius = 12
-        view.dodo.style.bar.hideAfterDelaySeconds = 3
-        view.dodo.style.bar.backgroundColor = #colorLiteral(red: 0.262835294, green: 0.8022480607, blue: 0.3886030316, alpha: 0.6978983275)
-        view.dodo.style.bar.animationShow = DodoAnimations.fade.show
-        view.dodo.style.bar.animationHide = DodoAnimations.fade.hide
-        view.dodo.topAnchor = view.safeAreaLayoutGuide.topAnchor
         view.dodo.success("Saved!")
     }
 }
