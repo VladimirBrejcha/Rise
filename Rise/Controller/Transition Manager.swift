19f9180cd3599ddee1600149924befd506857346
VladimirBrejcha <python2python@icloud.com>
2019-06-08 01:38:21 +0300

    transition file added

---
+++ Rise/Controller/Transition Manager.swift
@@ -0,0 +1,33 @@
+//
+//  Transition.swift
+//  Rise
+//
+//  Created by Vladimir Korolev on 08/06/2019.
+//  Copyright © 2019 VladimirBrejcha. All rights reserved.
+//
+
+import UIKit
+import SPStorkController
+
+class TranstitionManager {
+    
+    // MARK: Properties
+    var storyboard = UIStoryboard(name: Storyboard.name, bundle: nil)
+    
+    // MARK: Methods
+    func makeTransition(from senderController: UIViewController, to controllerID: String) {
+        
+        let controller = storyboard.instantiateViewController(withIdentifier: controllerID)
+        
+        let transitionDelegate = SPStorkTransitioningDelegate()
+        controller.transitioningDelegate = transitionDelegate
+        controller.modalPresentationStyle = .custom
+        
+        if senderController is SPStorkControllerDelegate {
+            transitionDelegate.storkDelegate = senderController as? SPStorkControllerDelegate
+        }
+        
+        senderController.present(controller, animated: true, completion: nil)
+    }
+    
+}
