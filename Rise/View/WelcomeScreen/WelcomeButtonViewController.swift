21ecdca61fdf889de9c9c2bb4f0cbc8af502d886
VladimirBrejcha <vladimirbrejcha@icloud.com>
2019-09-01 21:32:44 +0300

    after start button been pressed main app will become initial view controller

--- Rise/View/WelcomeScreen/WelcomeButtonViewController.swift
+++ Rise/View/WelcomeScreen/WelcomeButtonViewController.swift
@@ -13,18 +13,11 @@ class WelcomeButtonViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
     }
     
 
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    @IBAction func startButtonPressed(_ sender: UIButton) {
+        UserDefaults.welcomeScreenBeenShowed = true
     }
-    */
 
 }
