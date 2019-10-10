5f2746a6e0a7a7bd32f387f94607c66b0982a5c4
VladimirBrejcha <vladimirbrejcha@icloud.com>
2019-10-10 20:12:53 +0300

    button class created

--- Rise/UI/Button.swift
+++ Rise/UI/Button.swift
@@ -8,14 +8,35 @@
 
 import UIKit
 
+@IBDesignable
 class Button: UIButton {
 
-    /*
-    // Only override draw() if you perform custom drawing.
-    // An empty implementation adversely affects performance during animation.
-    override func draw(_ rect: CGRect) {
-        // Drawing code
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        sharedInit()
     }
-    */
+    
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+        sharedInit()
+    }
+    
+    private func sharedInit() {
+        backgroundColor = #colorLiteral(red: 1, green: 1, blue: 1, alpha: 0.07)
+        layer.cornerRadius = 12
+        addTarget(self, action: #selector(backToNormalSize(_:)), for: .touchCancel)
+        addTarget(self, action: #selector(backToNormalSize(_:)), for: .touchDragOutside)
+        addTarget(self, action: #selector(backToNormalSize(_:)), for: .touchUpInside)
+        addTarget(self, action: #selector(decreaseButtonSize(_:)), for: .touchDown)
+        addTarget(self, action: #selector(decreaseButtonSize(_:)), for: .touchDragInside)
+    }
+    
+    @objc func decreaseButtonSize(_ sender: UIButton) {
+        UIView.animate(withDuration: 0.1, animations: { sender.transform = CGAffineTransform(scaleX: 0.93, y: 0.93) })
+    }
+    
+    @objc func backToNormalSize(_ sender: UIButton) {
+        UIView.animate(withDuration: 0.1) { sender.transform = CGAffineTransform.identity }
+    }
 
 }
