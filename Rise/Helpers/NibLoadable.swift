f591d1996837eda360533b0651390fb137bdd5a4
VladimirBrejcha <vladimirbrejcha@icloud.com>
2019-10-10 20:12:11 +0300

    NibLoadable protocol added

--- Rise/Helpers/NibLoadable.swift
+++ Rise/Helpers/NibLoadable.swift
@@ -6,4 +6,28 @@
 //  Copyright © 2019 VladimirBrejcha. All rights reserved.
 //
 
-import Foundation
+import UIKit
+
+public protocol NibLoadable {
+    static var nibName: String { get }
+}
+
+public extension NibLoadable where Self: UIView {
+
+    static var nibName: String {
+        return String(describing: Self.self) // defaults to the name of the class implementing this protocol.
+    }
+
+    static var nib: UINib {
+        let bundle = Bundle(for: Self.self)
+        return UINib(nibName: Self.nibName, bundle: bundle)
+    }
+
+    func setupFromNib() {
+        guard let view = Self.nib.instantiate(withOwner: self, options: nil).first as? UIView else { fatalError("Error loading \(self) from nib") }
+        addSubview(view)
+        view.translatesAutoresizingMaskIntoConstraints = false
+        view.heightAnchor.constraint(equalTo: heightAnchor).isActive = true
+        view.widthAnchor.constraint(equalTo: widthAnchor).isActive = true
+    }
+}
