7e0e37ef8a5d36be0f6b65afd9f44757e02cbec4
VladimirBrejcha <vladimirbrejcha@icloud.com>
2019-10-10 21:48:48 +0300

    new files

---
+++ Rise/UI/LoadingView.swift
@@ -0,0 +1,21 @@
+//
+//  LoadingView.swift
+//  Rise
+//
+//  Created by Владимир Королев on 10.10.2019.
+//  Copyright © 2019 VladimirBrejcha. All rights reserved.
+//
+
+import UIKit
+
+class LoadingView: UIButton {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
