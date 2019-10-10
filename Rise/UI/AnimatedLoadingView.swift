63314997b2c5ff0b93b8899ecf58f4272e24f63e
VladimirBrejcha <vladimirbrejcha@icloud.com>
2019-10-10 21:50:20 +0300

    new loading view implemented instead of animationLoadingView

--- Rise/UI/AnimatedLoadingView.swift
+++ Rise/UI/AnimatedLoadingView.swift
@@ -16,7 +16,46 @@ class AnimatedLoadingView: DesignableContainerView {
         return layer
     }()
     
+    private var button: Button!
+    private var label: UILabel!
     
+    func setupLabel() {
+        let width = bounds.width / 2
+        let height = bounds.height / 3
+        let x = (bounds.width / 2) - (width / 2)
+        let y = (bounds.height / 2) - (height * 1.2)
+        let frame = CGRect(x: x, y: y, width: width, height: height)
+        label = UILabel(frame: frame)
+        label.textAlignment = .center
+        label.text = "loading failed"
+        label.alpha = 0
+        addSubview(label)
+    }
+    
+    func showLabel(_ show: Bool) {
+        UIView.animate(withDuration: 0.6, delay: 0, options: .allowUserInteraction, animations: {
+            self.label.alpha = show ? 1 : 0
+         })
+    }
+    
+    func setupButton() {
+        let width = bounds.width / 3
+        let height = bounds.height / 3
+        let x = (bounds.width / 2) - (width / 2)
+        let y = (bounds.height / 2) - (height / 5)
+        let frame = CGRect(x: x, y: y, width: width, height: height)
+        button = Button(frame: frame)
+        button.titleLabel?.font = UIFont.systemFont(ofSize: 14)
+        button.setTitle("try again", for: .normal)
+        button.alpha = 0
+        addSubview(button)
+    }
+    
+    func showButton(_ show: Bool) {
+        UIView.animate(withDuration: 0.6, delay: 0, options: .allowUserInteraction, animations: {
+            self.button.alpha = show ? 1 : 0
+         })
+    }
     
     func setupAnimationLayer() {
         layer.sublayers = nil
--- Rise/UI/LoadingView.swift
+++ Rise/UI/LoadingView.swift
@@ -8,14 +8,80 @@
 
 import UIKit
 
-class LoadingView: UIButton {
+@IBDesignable
+class LoadingView: UIButton, NibLoadable {
 
-    /*
-    // Only override draw() if you perform custom drawing.
-    // An empty implementation adversely affects performance during animation.
-    override func draw(_ rect: CGRect) {
-        // Drawing code
+    @IBOutlet weak var infoLabel: UILabel!
+    @IBOutlet weak var errorContainerView: UIView!
+    @IBOutlet weak var errorButton: Button!
+    @IBOutlet weak var errorLabel: UILabel!
+    @IBOutlet weak var animationView: UIView!
+    
+    private var animationManager: AnimationManager?
+    private let animationLayer: CALayer = {
+        let layer = CALayer()
+        layer.backgroundColor = UIColor.clear.cgColor
+        return layer
+    }()
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        setupFromNib()
     }
-    */
+    
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+        setupFromNib()
+    }
+    
+    func setupAnimationLayer() { // call it from outside once view layout been set to correctly setup animation
+        animationView.layer.sublayers = nil
+        animationLayer.frame = animationView.bounds
+        animationView.layer.addSublayer(animationLayer)
+        animationManager = AnimationManager(layer: animationLayer, tintColor: .white)
+        animationManager?.setupAnimation()
+        setNeedsDisplay()
+    }
 
+    @IBAction func errorButtonPressed(_ sender: Button) {
+        
+    }
+    
+    func showLoadingError() {
+        toggleViewAppearance(show: true, errorContainerView)
+    }
+    
+    func hideError() {
+        toggleViewAppearance(show: false, errorContainerView)
+    }
+    
+    func showLoading() {
+        animationManager?.startAnimating()
+        toggleViewAppearance(show: true, animationView)
+    }
+    
+    func hideLoading(completion: @escaping () -> Void) {
+        hideSelf {
+            self.animationManager?.stopAnimating()
+            completion()
+        }
+    }
+    
+    func showInfo(with text: String) {
+        infoLabel.text = text
+        toggleViewAppearance(show: true, infoLabel)
+    }
+    
+    func hideInfo() { toggleViewAppearance(show: false, infoLabel) }
+    
+    func hideSelf(completion: @escaping () -> Void) {
+        UIView.animate(withDuration: 0.6, delay: 0, options: .allowUserInteraction, animations: {
+            self.alpha = 0
+        }) { _ in  completion() }
+    }
+    
+    // MARK: - Private
+    private func toggleViewAppearance(show: Bool, _ view: UIView) {
+        UIView.animate(withDuration: 0.6, delay: 0, options: .allowUserInteraction, animations: { view.alpha = show ? 1 : 0 })
+    }
 }
