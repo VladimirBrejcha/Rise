51070edbf3660bbcf26b9f77b633d33cdac7daac
VladimirBrejcha <vladimirbrejcha@icloud.com>
2019-10-10 20:12:29 +0300

    added loading view to today plan cell

--- Rise/Stories/MainScreen/TodayCollectionViewCell/TodayCollectionViewCell.swift
+++ Rise/Stories/MainScreen/TodayCollectionViewCell/TodayCollectionViewCell.swift
@@ -19,26 +19,41 @@ class TodayCollectionViewCell: UICollectionViewCell {
     @IBOutlet weak var sunsetTimeLabel: UILabel!
     @IBOutlet weak var wakeUpTimeLabel: UILabel!
     @IBOutlet weak var toSleepTimeSleep: UILabel!
-    @IBOutlet weak var loadingView: AnimatedLoadingView!
+    @IBOutlet weak var sunLoadingView: AnimatedLoadingView!
     @IBOutlet weak var sunContainerView: UIView!
+    @IBOutlet weak var planContainerView: UIView!
+    @IBOutlet weak var planLoadingView: AnimatedLoadingView!
     
-    private var isDataLoaded: Bool = false
+    private var dataManager: CoreDataManager! {
+        return sharedCoreDataManager
+    }
+    
+    private var isSunDataLoaded: Bool = false
+    private var isPlanDataLoaded: Bool! {
+        return dataManager.currentPlan != nil
+    }
     
     var cellModel: TodayCellModel! {
         didSet {
             sunriseTimeLabel.text = cellModel.sunriseTime
             sunsetTimeLabel.text = cellModel.sunsetTime
-            isDataLoaded = cellModel.isDataLoaded
+            isSunDataLoaded = cellModel.isDataLoaded
         }
     }
     
     override func draw(_ rect: CGRect) {
         super.draw(rect)
-        loadingView.setupAnimationLayer()
+        sunLoadingView.setupAnimationLayer()
+        planLoadingView.setupAnimationLayer()
         
-        isDataLoaded
-            ? loadingView.hideLoading { UIView.animate(withDuration: 0.6, delay: 0, options: .allowUserInteraction,
+        isSunDataLoaded
+            ? sunLoadingView.hideLoading { UIView.animate(withDuration: 0.6, delay: 0, options: .allowUserInteraction,
                                                        animations: { self.sunContainerView.alpha = 1 }) }
-            : loadingView.showLoading()
+            : sunLoadingView.showLoading()
+        
+        isPlanDataLoaded
+            ? planLoadingView.hideLoading { UIView.animate(withDuration: 0.6, delay: 0, options: .allowUserInteraction,
+                                                       animations: { self.planContainerView.alpha = 1 }) }
+            : planLoadingView.showLoading()
     }
 }
