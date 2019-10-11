746cccd8713cc546c5ec653a43c709b94614d286
VladimirBrejcha <vladimirbrejcha@icloud.com>
2019-10-11 21:27:24 +0300

    autosetteble table view subclass implemeted

--- Rise/UI/TableView.swift
+++ Rise/UI/TableView.swift
@@ -6,4 +6,29 @@
 //  Copyright © 2019 VladimirBrejcha. All rights reserved.
 //
 
-import Foundation
+import UIKit
+
+class TableView: UITableView {
+    var cellID: String {
+        return ""
+    }
+    var nibName: String {
+        return ""
+    }
+    
+    var nib: UINib {
+        return UINib(nibName: nibName, bundle: nil)
+    }
+    
+    override init(frame: CGRect, style: UITableView.Style) {
+        super.init(frame: frame, style: style)
+        sharedInit()
+    }
+    
+    required init?(coder: NSCoder) {
+        super.init(coder: coder)
+        sharedInit()
+    }
+    
+    private func sharedInit() { register(nib, forCellReuseIdentifier: cellID) }
+}
