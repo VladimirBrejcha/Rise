5a543ba7340bd506620bf7dfd8681539b2cd1a68
VladimirBrejcha <vladimirbrejcha@icloud.com>
2019-10-14 21:29:21 +0300

    file structure changes

---
+++ Rise/Stories/MainScreen/MainScreenPresenter.swift
@@ -0,0 +1,9 @@
+//
+//  MainScreenPresenter.swift
+//  Rise
+//
+//  Created by Владимир Королев on 14.10.2019.
+//  Copyright © 2019 VladimirBrejcha. All rights reserved.
+//
+
+import Foundation
---
+++ Rise/Stories/MainScreen/TodayCollectionViewCell/TodayCollectionViewDataSource.swift
@@ -0,0 +1,9 @@
+//
+//  TodayCollectionViewDataSource.swift
+//  Rise
+//
+//  Created by Владимир Королев on 14.10.2019.
+//  Copyright © 2019 VladimirBrejcha. All rights reserved.
+//
+
+import Foundation
