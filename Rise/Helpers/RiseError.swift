0edca63d1a215c8941d1046441f65a91bc9618a8
VladimirBrejcha <vladimirbrejcha@icloud.com>
2019-10-22 23:01:19 +0300

    new RiseError

--- Rise/Helpers/RiseError.swift
+++ Rise/Helpers/RiseError.swift
@@ -11,23 +11,25 @@ import Foundation
 fileprivate let errorDomain = Bundle.main.bundleIdentifier!
 
 enum RiseError: Int {
-    case unknownError   = 5999
-    case cantFormatDate = 6000
-    case cantParseJSON  = 6001
-    case cantBuildJSON  = 6002
-    case noDataReceived = 6003
-    case cantBuildURL   = 6004
-    case noDataFound    = 6005
+    case unknownError      = 5999
+    case cantFormatDate    = 6000
+    case cantParseJSON     = 6001
+    case cantBuildJSON     = 6002
+    case noDataReceived    = 6003
+    case cantBuildURL      = 6004
+    case noDataFound       = 6005
+    case noLocationArrived = 6006
     
     private var description: String {
         switch self {
-        case .unknownError  :  return "Unknown error"
-        case .cantFormatDate:  return "Could'nt formate Date"
-        case .cantParseJSON :  return "Could'nt parce JSON"
-        case .cantBuildJSON :  return "Could'nt build JSON from Data"
-        case .noDataReceived:  return "Did'nt receive any data from request"
-        case .cantBuildURL  :  return "Could'nt build URL"
-        case .noDataFound   :  return "Could'nt find any data"
+        case .unknownError      : return "Unknown error"
+        case .cantFormatDate    : return "Could'nt formate Date"
+        case .cantParseJSON     : return "Could'nt parce JSON"
+        case .cantBuildJSON     : return "Could'nt build JSON from Data"
+        case .noDataReceived    : return "Did'nt receive any data from request"
+        case .cantBuildURL      : return "Could'nt build URL"
+        case .noDataFound       : return "Could'nt find any data"
+        case .noLocationArrived : return "Location data did'nt arrive"
         }
     }
     
@@ -71,5 +73,10 @@ extension RiseError {
     static func errorNoDataFound() -> NSError {
         return NSError(domain: errorDomain, code: RiseError.noDataFound.rawValue,
                        userInfo: RiseError.noDataFound.localizedDescriptionInfo)
+    }
+    
+    static func errorNoLocationArrived() -> NSError {
+        return NSError(domain: errorDomain, code: RiseError.noLocationArrived.rawValue,
+                       userInfo: RiseError.noLocationArrived.localizedDescriptionInfo)
     }
 }
