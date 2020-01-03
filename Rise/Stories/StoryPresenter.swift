9315a499de7848cd75b8a0d9355ad0c9731c4d7a
VladimirBrejcha <vladimirbrejcha@icloud.com>
2020-01-04 01:48:02 +0300

    StoryPresenter created and implemented

--- Rise/Stories/StoryPresenter.swift
+++ Rise/Stories/StoryPresenter.swift
@@ -6,4 +6,34 @@
 //  Copyright © 2019 VladimirBrejcha. All rights reserved.
 //
 
-import Foundation
+import UIKit
+
+enum PresentationStyle {
+    case fullScreen
+    case modal
+    case overContext
+    case none
+}
+
+final class StoryPresenter {
+    class func present(story: UIViewController,
+                       with style: PresentationStyle,
+                       presentingController: UIViewController,
+                       animated: Bool = true,
+                       completion: (() -> Void)? = nil) {
+        switch style {
+        case .fullScreen:
+            presentingController.modalPresentationStyle = .fullScreen
+            presentingController.present(story, animated: animated, completion: completion)
+        case .modal:
+            presentingController.modalPresentationStyle = .pageSheet
+            presentingController.present(story, animated: animated, completion: completion)
+        case .overContext:
+            presentingController.modalPresentationStyle = .overCurrentContext
+            presentingController.present(story, animated: animated, completion: completion)
+        case .none:
+            presentingController.modalPresentationStyle = .none
+            presentingController.present(story, animated: animated, completion: completion)
+        }
+    }
+}
