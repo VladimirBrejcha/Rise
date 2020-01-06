043a16c040af45f073b2e6fbf010fb8fab02fe1a
VladimirBrejcha <vladimirbrejcha@icloud.com>
2020-01-06 18:02:14 +0300

    file renaming

--- Rise/Stories/Presenter.swift
+++ Rise/Stories/Presenter.swift
@@ -15,7 +15,7 @@ enum PresentationStyle {
     case none
 }
 
-final class StoryPresenter {
+final class Presenter {
     class func present(story: UIViewController,
                        with style: PresentationStyle,
                        presentingController: UIViewController,
