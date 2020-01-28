bcdcf4bdba78b981784e1f7d8ea971696f7ef156
VladimirBrejcha <vladimirbrejcha@icloud.com>
2020-01-28 16:06:01 +0300

    loading view improvements and refactoring

--- Rise/UI/LoadingView.swift
+++ Rise/UI/LoadingView.swift
@@ -8,26 +8,18 @@
 
 import UIKit
 
-protocol LoadingViewDelegate: AnyObject {
-    func repeatButtonPressed()
-}
-
 @IBDesignable
-class LoadingView: UIButton, NibLoadable {
-    weak var delegate: LoadingViewDelegate?
-
-    @IBOutlet weak var infoLabel: UILabel!
-    @IBOutlet weak var errorContainerView: UIView!
-    @IBOutlet weak var errorButton: Button!
-    @IBOutlet weak var errorLabel: UILabel!
-    @IBOutlet weak var animationView: UIView!
+final class LoadingView: UIButton, NibLoadable {
+    @IBOutlet private weak var infoLabel: UILabel!
+    @IBOutlet private weak var errorContainerView: UIView!
+    @IBOutlet private weak var repeatButton: Button!
+    @IBOutlet private weak var errorLabel: UILabel!
+    @IBOutlet private weak var animationView: UIView!
     
     private var animationManager: AnimationManager?
-    private let animationLayer: CALayer = {
-        let layer = CALayer()
-        layer.backgroundColor = UIColor.clear.cgColor
-        return layer
-    }()
+    private var animationLayer: CALayer?
+    
+    var repeatButtonHandler: (() -> Void)?
     
     override init(frame: CGRect) {
         super.init(frame: frame)
@@ -38,60 +30,58 @@ class LoadingView: UIButton, NibLoadable {
         super.init(coder: aDecoder)
         setupFromNib()
     }
-    
-    func setupAnimationLayer() { // call it from outside once view layout been set to correctly setup animation
-        animationView.layer.sublayers = nil
-        animationLayer.frame = animationView.bounds
-        animationView.layer.addSublayer(animationLayer)
-        animationManager = AnimationManager(layer: animationLayer, tintColor: .white)
-        animationManager?.setupAnimation()
-        setNeedsDisplay()
-    }
 
     @IBAction func errorButtonPressed(_ sender: Button) {
-        delegate?.repeatButtonPressed()
+        repeatButtonHandler?()
     }
     
-    // MARK: - Error
-    func showLoadingError() {
-        toggleViewAppearance(show: true, errorContainerView)
+    // MARK: - Error view -
+    func showError(_ show: Bool) {
+        showView(show, view: errorContainerView)
     }
     
-    func hideError() {
-        toggleViewAppearance(show: false, errorContainerView)
-    }
-    
-    // MARK: - Loadiing
-    func showLoading(completion: (() -> Void)? = nil) {
-        animationManager?.startAnimating()
-        toggleViewAppearance(show: true, animationView, completion: completion)
-    }
-    
-    func hideLoading(completion: (() -> Void)? = nil) {
-        toggleViewAppearance(show: false, animationView) {
-            self.animationManager?.stopAnimating()
-            completion?()
+    // MARK: - Loadiing animation -
+    func showLoading(_ show: Bool, completion: (() -> Void)? = nil) {
+        if animationLayer == nil {
+            setupAnimationLayer()
+        }
+        
+        if show {
+            animationManager?.animate(true)
+            showView(true, view: animationView, completion: completion)
+        } else {
+            showView(false, view: animationView) {
+                 self.animationManager?.animate(false)
+                 completion?()
+             }
         }
     }
     
-    // MARK: - Info
+    private func setupAnimationLayer() {
+        animationView.layer.sublayers = nil
+        let layer = CALayer()
+        layer.backgroundColor = UIColor.clear.cgColor
+        layer.frame = animationView.bounds
+        animationView.layer.addSublayer(layer)
+        
+        self.animationLayer = layer
+        animationManager = AnimationManager(with: .pulsingCircles)
+        animationManager?.setupAnimation(on: layer)
+        setNeedsDisplay()
+    }
+    
+    // MARK: - Info label -
     func showInfo(with text: String) {
         infoLabel.text = text
-        toggleViewAppearance(show: true, infoLabel)
+        showView(true, view: infoLabel)
     }
     
     func hideInfo(completion: (() -> Void)? = nil) {
-        toggleViewAppearance(show: false, infoLabel, completion: completion)
+        showView(false, view: infoLabel, completion: completion)
     }
     
-    // MARK: - Private
-    private func hideSelf(completion: @escaping () -> Void) {
-        UIView.animate(withDuration: 0.6, delay: 0, options: .allowUserInteraction, animations: {
-            self.alpha = 0
-        }) { _ in  completion() }
-    }
-    
-    private func toggleViewAppearance(show: Bool, _ view: UIView, completion: (() -> Void)? = nil) {
+    // MARK: - Private -
+    private func showView(_ show: Bool, view: UIView, completion: (() -> Void)? = nil) {
         UIView.animate(withDuration: 0.6, delay: 0, options: .allowUserInteraction, animations: {
             view.alpha = show ? 1 : 0
         }) { _ in completion?() }
