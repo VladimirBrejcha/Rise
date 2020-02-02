5b90639c789f8f2dc6f3eefb0c43eaf13d8733c2
VladimirBrejcha <vladimirbrejcha@icloud.com>
2020-02-02 21:14:13 +0300

    changePlanAssembler created

--- Rise/Stories/ChangePlan/ChangePlanAssembler.swift
+++ Rise/Stories/ChangePlan/ChangePlanAssembler.swift
@@ -7,3 +7,24 @@
 //
 
 import Foundation
+
+final class ChangePlanAssembler: StoryAssembler {
+    typealias View = ChangePlanViewController
+    typealias ViewInput = ChangePlanViewInput
+    typealias ViewOutput = ChangePlanViewOutput
+    
+    func assemble() -> ChangePlanViewController {
+        let controller = Storyboard.changePlan.instantiateViewController(of: ChangePlanViewController.self)
+        controller.output = assemble(view: controller)
+        return controller
+    }
+    
+    func assemble(view: ChangePlanViewInput) -> ChangePlanViewOutput {
+        let presenter = ChangePlanPresenter(view: view, updatePlan: assemble())
+        return presenter
+    }
+    
+    private func assemble() -> UpdatePlan {
+        return UpdatePlan(planRepository: DataLayer.personalPlanRepository)
+    }
+}
