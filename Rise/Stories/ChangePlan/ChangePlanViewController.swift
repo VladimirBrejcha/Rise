8afe135bf528b24df0d1cd056238926ec2662120
VladimirBrejcha <vladimirbrejcha@icloud.com>
2020-02-02 21:13:50 +0300

    changePlanViewController added

--- Rise/Stories/ChangePlan/ChangePlanViewController.swift
+++ Rise/Stories/ChangePlan/ChangePlanViewController.swift
@@ -6,4 +6,47 @@
 //  Copyright © 2020 VladimirBrejcha. All rights reserved.
 //
 
-import Foundation
+import UIKit
+
+protocol ChangePlanViewInput: AnyObject {
+    
+}
+
+protocol ChangePlanViewOutput: ViewOutput {
+    
+}
+
+final class ChangePlanViewController: UIViewController, ChangePlanViewInput {
+    var output: ChangePlanViewOutput!
+    
+    @IBOutlet private weak var saveButton: Button!
+    @IBOutlet private weak var closeButton: UIButton!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        let backgroundView = GradientHelper.makeDefaultStaticGradient(for: view.bounds)
+        view.addSubview(backgroundView)
+        view.sendSubviewToBack(backgroundView)
+        
+        output.viewDidLoad()
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        
+        output.viewWillAppear()
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        
+        output.viewDidAppear()
+    }
+
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        
+        output.viewWillDisappear()
+    }
+}
