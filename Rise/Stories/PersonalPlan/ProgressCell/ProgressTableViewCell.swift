4defd714c145ed5cf6d27a10c812b4a5fe648a7c
VladimirBrejcha <vladimirbrejcha@icloud.com>
2020-02-02 19:45:15 +0300

    progressCell improvements for paused plan

--- Rise/Stories/PersonalPlan/ProgressCell/ProgressTableViewCell.swift
+++ Rise/Stories/PersonalPlan/ProgressCell/ProgressTableViewCell.swift
@@ -8,19 +8,36 @@
 
 import UIKit
 
-class ProgressTableViewCell: UITableViewCell {
+final class ProgressTableViewCell: UITableViewCell {
     @IBOutlet weak var startProgressLabel: UILabel!
     @IBOutlet weak var endProgressLabel: UILabel!
     @IBOutlet weak var centerProgressLabel: UILabel!
-    @IBOutlet weak var progressBarView: ProgressBar!
+    @IBOutlet private weak var progressBarView: ProgressBar!
+    @IBOutlet private weak var cellContentView: UIView!
+    @IBOutlet private weak var loadingView: LoadingView!
+    
     var progress: CGFloat = 0.0 {
-        willSet {
-            progressBarView.showProgress(progress: newValue)
+        didSet {
+            progressBarView.showProgress(progress: progress)
+        }
+    }
+    
+    var paused: Bool = true {
+        didSet {
+            // TODO: - animation doesnt work
+            UIView.animate(withDuration: 0.15) {
+                self.cellContentView.alpha = self.paused ? 0 : 1
+                self.loadingView.alpha = self.paused ? 1 : 0
+            }
+            if paused {
+                loadingView.showInfo(with: "Your plan is paused")
+            }
         }
     }
     
     override func awakeFromNib() {
         super.awakeFromNib()
+        loadingView.containerView.background = .clear
         progressBarView.showProgress(progress: progress)
     }
 
