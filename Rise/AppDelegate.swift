f0c395ed6ab224c077d97a1bcb8086ffd98eb1c0
VladimirBrejcha <vladimirbrejcha@icloud.com>
2020-02-16 14:41:26 +0300

    clean up AppDelegate

--- Rise/AppDelegate.swift
+++ Rise/AppDelegate.swift
@@ -7,32 +7,13 @@
 //
 
 import UIKit
-import CoreData
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
     
-    // MARK: Properties
     var window: UIWindow?
     
-    // MARK: LifeCycle
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-        
-//        setupInitialController()
-        
         return true
     }
-    
-    func applicationWillTerminate(_ application: UIApplication) {
-//        coreDataManager.persistentContainer.saveContext() // TODO
-    }
-    
-    private func setupInitialController() {
-        window = UIWindow(frame: UIScreen.main.bounds)
-//        window?.rootViewController = UserDefaults.welcomeScreenBeenShowed
-//            ? UIStoryboard.mainAppController
-//            : UIStoryboard.mainAppController
-        window?.makeKeyAndVisible()
-    }
-    
 }
