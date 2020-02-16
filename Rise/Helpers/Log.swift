5b24c68b0f3c281a9d959530366d0c120604955e
VladimirBrejcha <vladimirbrejcha@icloud.com>
2020-02-16 14:42:27 +0300

    Log reworked

--- Rise/Helpers/Log.swift
+++ Rise/Helpers/Log.swift
@@ -8,6 +8,20 @@
 
 import Foundation
 
-func log(_ message: String, _ file: String = #file, _ line: Int = #line, _ function: String = #function) {
-    print("[\(file):\(line)] \(function) - \(message)")
+func log(_ type: LogType, with message: String, _ file: String = #file, _ line: Int = #line, _ function: String = #function) {
+    print("[\(type.prefix)] \n\(file):\(line) - \(function) \n\(message)")
+}
+
+enum LogType {
+    case error
+    case warning
+    case info
+    
+    var prefix: String {
+        switch self {
+        case .error: return "RISE ERROR"
+        case .warning: return "RISE WARNING"
+        case .info: return "RISE INFO"
+        }
+    }
 }
