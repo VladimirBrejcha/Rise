644b5309bfae1ee3f1e8ce0a607a37128d47783e
VladimirBrejcha <vladimirbrejcha@icloud.com>
2020-03-07 17:51:50 +0300

    fixed crash because of wrong named storyboard

--- Rise/Storyboards/Storyboard.swift
+++ Rise/Storyboards/Storyboard.swift
@@ -11,7 +11,7 @@ import UIKit
 struct Storyboard {
     static let main: UIStoryboard = UIStoryboard(name: "Main", bundle: nil)
     static let changePlan: UIStoryboard = UIStoryboard(name: "ChangePlan", bundle: nil)
-    static let setupPlan: UIStoryboard = UIStoryboard(name: "SetupPlan", bundle: nil)
+    static let setupPlan: UIStoryboard = UIStoryboard(name: "CreatePlan", bundle: nil)
     static let settings: UIStoryboard = UIStoryboard(name: "Settings", bundle: nil)
     static let popUp: UIStoryboard = UIStoryboard(name: "Confirmation", bundle: nil)
 }
