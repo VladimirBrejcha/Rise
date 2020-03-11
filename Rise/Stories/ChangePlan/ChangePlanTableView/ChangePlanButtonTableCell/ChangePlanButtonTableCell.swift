a2dae010dae891c44ee3aad93412a4b2e552e9d4
VladimirBrejcha <vladimirbrejcha@icloud.com>
2020-03-11 16:16:35 +0300

    new files

---
+++ Rise/Stories/ChangePlan/ChangePlanTableView/ChangePlanButtonTableCell/ChangePlanButtonTableCell.swift
@@ -0,0 +1,24 @@
+//
+//  ChangePlanButtonTableCell.swift
+//  Rise
+//
+//  Created by Владимир Королев on 11.03.2020.
+//  Copyright © 2020 VladimirBrejcha. All rights reserved.
+//
+
+import UIKit
+
+class ChangePlanButtonTableCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
