28b557532e3cf2ceab1231c6ca96ea471f9c0d23
VladimirBrejcha <vladimirbrejcha@icloud.com>
2020-08-18 14:40:17 +0300

    fix Button animationb

--- Rise/UI/Button.swift
+++ Rise/UI/Button.swift
@@ -28,19 +28,29 @@ class Button: UIButton, PropertyAnimatable, TouchObservable {
     private func sharedInit() {
         backgroundColor = Color.defaultButtonBackground
         layer.cornerRadius = 12
+        addTarget(self, action: #selector(touchDown(_:)), for: .touchDown)
+        addTarget(self, action: #selector(touchUp(_:)), for: .touchUpInside)
+        addTarget(self, action: #selector(backToNormalSize(_:)), for: [.touchDragOutside, .touchCancel, .touchUpInside])
+        addTarget(self, action: #selector(decreaseButtonSize(_:)), for: [.touchDown, .touchDragInside])
         setTitleColor(Color.normalTitle, for: .normal)
         setTitleColor(Color.disabledTitle, for: .disabled)
     }
     
-    @objc func touchDown(_ sender: UIButton) {
+    @objc private func touchDown(_ sender: UIButton) {
         touchDownObserver?(self)
+    }
+    
+    @objc private func touchUp(_ sender: UIButton) {
+        touchUpObserver?(self)
+    }
+    
+    @objc private func decreaseButtonSize(_ sender: UIButton) {
         animate {
             sender.transform = CGAffineTransform(scaleX: 0.93, y: 0.93)
         }
     }
     
-    @objc func touchUp(_ sender: UIButton) {
-        touchUpObserver?(self)
+    @objc private func backToNormalSize(_ sender: UIButton) {
         animate {
             sender.transform = CGAffineTransform.identity
         }
