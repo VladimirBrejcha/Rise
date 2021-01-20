e7c00b4506f6f90975748d0ffe90f478ca66af6f
VladimirBrejcha <vladimirbrejcha@icloud.com>
2021-01-20 23:23:54 +0300

    tabBar changed with new appearance

--- Rise/UI/TabBarController/CustomTabBarController.swift
+++ Rise/UI/TabBarController/CustomTabBarController.swift
@@ -8,60 +8,34 @@
 
 import UIKit
 
-final class CustomTabBarController: UITabBarController, UITabBarControllerDelegate, BackgroundSettable {
-    
-    private let middleButtonBackgroundImageView = UIImageView()
+final class CustomTabBarController: UITabBarController, UITabBarControllerDelegate {
+    private lazy var appeareance: UITabBarAppearance = {
+        let appearance = UITabBarAppearance()
+        appearance.configureWithTransparentBackground()
+        appearance.backgroundImage = #imageLiteral(resourceName: "tabBarBack")
+        appearance.stackedItemPositioning = .fill
+        appearance.compactInlineLayoutAppearance = makeItemAppearance(for: .compactInline)
+        appearance.inlineLayoutAppearance = makeItemAppearance(for: .inline)
+        appearance.stackedLayoutAppearance = makeItemAppearance(for: .stacked)
+        return appearance
+    }()
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-        delegate = self
-        
-        setBackground(GradientHelper.makeAnimatedGradientView(frame: view.bounds))
-        
-        viewControllers = [Story.plan(),
-                           Story.today(),
-                           Story.settings()]
-        
+        viewControllers = [Story.plan(), Story.today(), Story.settings()]
         selectedIndex = 1
+        tabBar.standardAppearance = appeareance
+        delegate = self
     }
-    
-    override func viewWillAppear(_ animated: Bool) {
-        super.viewWillAppear(animated)
-        
-        tabBar.items![1].image = #imageLiteral(resourceName: "sleepIcon").withRenderingMode(.alwaysOriginal)
-        tabBar.items![1].selectedImage = #imageLiteral(resourceName: "sleepIconPressed").withRenderingMode(.alwaysOriginal)
-        
-        for tabBarItem in tabBar.items! {
-            tabBarItem.imageInsets = UIEdgeInsets(top: 6, left: 0, bottom: -6, right: 0)
-            tabBarItem.title = nil
-        }
-        
-        UITabBar.appearance().backgroundImage = Color.defaultContainerBackground.image()
-        UITabBar.appearance().shadowImage = UIImage()
-    }
-    
-    override func viewSafeAreaInsetsDidChange() {
-        super.viewSafeAreaInsetsDidChange()
-        
-        middleButtonBackgroundImageView.image = #imageLiteral(resourceName: "Union2")
-        
-        tabBar.addSubview(middleButtonBackgroundImageView)
-        tabBar.sendSubviewToBack(middleButtonBackgroundImageView)
-        
-        middleButtonBackgroundImageView.translatesAutoresizingMaskIntoConstraints = false
-        middleButtonBackgroundImageView.topAnchor.constraint(equalTo: tabBar.topAnchor,
-                                                             constant: 6).isActive = true
-        middleButtonBackgroundImageView.bottomAnchor.constraint(equalTo: tabBar.bottomAnchor,
-                                                                constant: -(6 + view.safeAreaInsets.bottom)).isActive = true
-        middleButtonBackgroundImageView.centerXAnchor.constraint(equalTo: tabBar.centerXAnchor).isActive = true
-    }
-    
-    override func tabBar(_ tabBar: UITabBar, didSelect item: UITabBarItem) {
-        middleButtonBackgroundImageView.image
-            = item == tabBar.items![1]
-            ?  #imageLiteral(resourceName: "Union2")
-            :  #imageLiteral(resourceName: "Union")
+
+    private func makeItemAppearance(for style: UITabBarItemAppearance.Style) -> UITabBarItemAppearance {
+        let appearance = UITabBarItemAppearance(style: style)
+        appearance.configureWithDefault(for: style)
+        appearance.normal.iconColor = UIColor.white.withAlphaComponent(0.85)
+        appearance.normal.titleTextAttributes = [.foregroundColor: UIColor.white.withAlphaComponent(0)]
+        appearance.selected.titleTextAttributes = [.foregroundColor: UIColor.white]
+        appearance.selected.iconColor = .white
+        return appearance
     }
     
     // MARK: - UITabBarControllerDelegate -
@@ -71,14 +45,5 @@ final class CustomTabBarController: UITabBarController, UITabBarControllerDelega
         to toVC: UIViewController
     ) -> UIViewControllerAnimatedTransitioning? {
         CustomTransition(viewControllers: tabBarController.viewControllers)
-    }
-}
-
-fileprivate extension UIColor {
-    func image(_ size: CGSize = CGSize(width: 1, height: 1)) -> UIImage {
-        return UIGraphicsImageRenderer(size: size).image { rendererContext in
-            self.setFill()
-            rendererContext.fill(CGRect(origin: .zero, size: size))
-        }
     }
 }
