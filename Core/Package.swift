ad7bcd8f102206e278bca33f6063c843123ea47e
Vladimir Brejcha <vladimirBrejcha@icloud.com>
2022-07-17 01:57:20 +0700

    Refactor Packages.swift

--- Core/Package.swift
+++ Core/Package.swift
@@ -1,29 +1,23 @@
 // swift-tools-version: 5.6
-// The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
 
 let package = Package(
-    name: "Core",
-    platforms: [.iOS(.v13)],
-    products: [
-        // Products define the executables and libraries a package produces, and make them visible to other packages.
-        .library(
-            name: "Core",
-            targets: ["Core"]),
-    ],
-    dependencies: [
-        // Dependencies declare other packages that this package depends on.
-        // .package(url: /* package url */, from: "1.0.0"),
-    ],
-    targets: [
-        // Targets are the basic building blocks of a package. A target can define a module or a test suite.
-        // Targets can depend on other targets in this package, and on products in packages this package depends on.
-        .target(
-            name: "Core",
-            dependencies: []),
-        .testTarget(
-            name: "CoreTests",
-            dependencies: ["Core"]),
-    ]
+  name: "Core",
+  platforms: [.iOS(.v14)],
+  products: [
+    .library(
+      name: "Core",
+      targets: ["Core"]),
+  ],
+  dependencies: [
+  ],
+  targets: [
+    .target(
+      name: "Core",
+      dependencies: []),
+    .testTarget(
+      name: "CoreTests",
+      dependencies: ["Core"]),
+  ]
 )
--- DataLayer/Package.swift
+++ DataLayer/Package.swift
@@ -1,30 +1,24 @@
 // swift-tools-version: 5.6
-// The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
 
 let package = Package(
-    name: "DataLayer",
-    platforms: [.iOS(.v13)],
-    products: [
-        // Products define the executables and libraries a package produces, and make them visible to other packages.
-        .library(
-            name: "DataLayer",
-            targets: ["DataLayer"]),
-    ],
-    dependencies: [
-      .package(path: "Core/")
-        // Dependencies declare other packages that this package depends on.
-        // .package(url: /* package url */, from: "1.0.0"),
-    ],
-    targets: [
-        // Targets are the basic building blocks of a package. A target can define a module or a test suite.
-        // Targets can depend on other targets in this package, and on products in packages this package depends on.
-        .target(
-            name: "DataLayer",
-            dependencies: ["Core"]),
-        .testTarget(
-            name: "DataLayerTests",
-            dependencies: ["DataLayer"]),
-    ]
+  name: "DataLayer",
+  platforms: [.iOS(.v14)],
+  products: [
+    .library(
+      name: "DataLayer",
+      targets: ["DataLayer"]),
+  ],
+  dependencies: [
+    .package(path: "Core/")
+  ],
+  targets: [
+    .target(
+      name: "DataLayer",
+      dependencies: ["Core"]),
+    .testTarget(
+      name: "DataLayerTests",
+      dependencies: ["DataLayer"]),
+  ]
 )
