c18f2b9f518a5744be1d2dd4edad466f46bf16fb
Artem <artchzhen@gmail.com>
2023-05-31 20:26:45 +0900

    added 4 arrays

--- Rise/Service/NotificationManager.swift
+++ Rise/Service/NotificationManager.swift
@@ -126,3 +126,54 @@ func notifyIsTimeToSleep () {
     let request = UNNotificationRequest(identifier: UUID().uuidString, content: content, trigger: trigger)
     center.add(request)
 }
+
+
+//
+
+protocol Notification {
+    
+}
+
+let notificationTitles: [String] = ["Sleepy Time Awaits",
+                                    "Ready for Dreamland?",
+                                    "Dream Journey Ahead",
+                                    "Time to Recharge",
+                                    "Off to the Land of Nod",
+                                    "Embrace the Night",
+                                    "Your Dreams Await",
+                                    "Healing Slumber Calls",
+                                    "Step into Tranquility",
+                                    "Unleash Your Rest"]
+
+let notificationDescriptions: [String] = ["Sleep is the best meditation. Are you ready to recharge for a better tomorrow?",
+                                          "Don't count sheep, count on us for a better sleep. Shall we begin?",
+                                          "Your dreams are waiting for you. Ready to explore them with us?",
+                                          "The stars are shining, and the moon is bright. It's the perfect time to say goodnight. Shall we?",
+                                          "Empower your tomorrow with the sleep of today. Ready to rest your mind?",
+                                          "Your body heals during sleep. Let's get some healing done, shall we?",
+                                          "Sweet dreams are made of these moments of peace. Ready to dive into tranquility?",
+                                          "Tonight's forecast? 100% chance of sleep. Ready for a blissful journey?",
+                                          "A well-rested you is the best you. Are you ready to wake up refreshed?",
+                                          "The night is young, and dreams are waiting. Ready to join the world of dreams?"]
+
+let cancelButtons: [String] = ["Maybe Later",
+                               "Not Now",
+                               "Perhaps Later",
+                               "Maybe in a Bit",
+                               "Later",
+                               "Not Yet",
+                               "Later Maybe",
+                               "Soon",
+                               "In a While",
+                               "A Bit Later"]
+
+let acceptButtons: [String] = ["Start Sleeping",
+                               "Yes, Let's Sleep",
+                               "Begin Dreaming",
+                               "Goodnight",
+                               "Start Resting",
+                               "Time to Heal",
+                               "Dive In",
+                               "Start Journey",
+                               "Refresh Now",
+                               "Join Dreams"]
