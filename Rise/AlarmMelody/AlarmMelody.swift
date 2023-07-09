db46010abc6a99c02ce176ca5eae9fdd5f5a2b9b
Vladislav <law.vladislav@gmail.com>
2023-07-09 13:46:34 +0300

    added playingEnd Notification

--- Rise/AlarmMelody/AlarmMelody.swift
+++ Rise/AlarmMelody/AlarmMelody.swift
@@ -19,7 +19,7 @@ final class AlarmMelody {
         guard let url = melody.path else { return nil }
         self.alarmPlayer = AVPlayer(url: url)
         smoothSound()
-        
+        seekAfterPlayinEnd()
     }
 
     // MARK: - Public methods
@@ -45,6 +45,7 @@ final class AlarmMelody {
         } catch {
             print("Audio session error pause")
         }
+        guard alarmPlayer.volume < 1 else { return }
         alarmPlayer.removeTimeObserver(timeObserver as Any)
     }
 
@@ -52,13 +53,31 @@ final class AlarmMelody {
 
     private func smoothSound() {
         alarmPlayer.volume = 0.0
-       timeObserver = alarmPlayer.addPeriodicTimeObserver(forInterval: CMTime(seconds: 2, preferredTimescale: 1000),
-                                            queue: DispatchQueue.main) { _ in
-            guard self.alarmPlayer.volume < 1 else { return }
-                self.alarmPlayer.volume += 0.1
-        }
+        timeObserver = alarmPlayer.addPeriodicTimeObserver(
+            forInterval: CMTime(seconds: 0.25,
+                                preferredTimescale: CMTimeScale(NSEC_PER_SEC)),
+            queue: DispatchQueue.main) { _ in
+                guard self.alarmPlayer.volume < 1 else {
+                    self.alarmPlayer.removeTimeObserver(self.timeObserver as Any)
+                    return }
+                print(self.alarmPlayer.volume)
+                self.alarmPlayer.volume += 0.025
+            }
     }
 
+    // MARK: - NotificationCenter
+
+    private func seekAfterPlayinEnd() {
+        NotificationCenter.default.addObserver(self,
+                                               selector: #selector(actionAfterStopAudio),
+                                               name: NSNotification.Name.AVPlayerItemDidPlayToEndTime,
+                                               object: nil)
+    }
+
+    @objc private func actionAfterStopAudio() {
+        alarmPlayer.seek(to: .zero)
+        alarmPlayer.play()
+    }
     
 }
 
