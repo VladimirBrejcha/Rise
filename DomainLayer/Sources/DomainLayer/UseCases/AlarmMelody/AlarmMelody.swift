0eca382972a5bec31f31c7a06495d8b4ecc8458b
Vladislav <law.vladislav@gmail.com>
2023-07-11 19:13:38 +0300

    added alternative AlarmMelodys

--- DomainLayer/Sources/DomainLayer/UseCases/AlarmMelody/AlarmMelody.swift
+++ DomainLayer/Sources/DomainLayer/UseCases/AlarmMelody/AlarmMelody.swift
@@ -10,22 +10,31 @@ import Foundation
 
 enum AlarmMelody {
     case defaultMelody
+    case cockMelody
+    case erokiaMelody
+    case oldAlarmMelody
+    case tiktakMelody
     
     var name: String {
         switch self {
         case .defaultMelody: return "DefaultMelody"
+        case .cockMelody: return "cockMelody"
+        case .erokiaMelody: return "erokiaMelody"
+        case .oldAlarmMelody: return "oldAlarmMelody"
+        case .tiktakMelody: return "tiktakMelody"
         }
     }
     
     var type: String {
         switch self {
-        case .defaultMelody: return "wav"
+        case .erokiaMelody: return "mp3"
+        default: return "wav"
         }
     }
     
     var path: URL? {
         switch self {
-        case .defaultMelody:
+        default:
             guard let stringPath = Bundle.main.path(forResource: name, ofType: type) else { assertionFailure("Name melody warning") ; return nil }
             return URL(fileURLWithPath: stringPath)
         }
