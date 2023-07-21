d5b91a0d892e9271618745552e0f45945e97dc83
Vladimir Brejcha <vladimirBrejcha@icloud.com>
2023-07-21 12:47:19 +0800

    Correctly handle 1 minute/1 hour (instead of 1 minutes/1 hours)

--- Core/Sources/Core/Double+Utils.swift
+++ Core/Sources/Core/Double+Utils.swift
@@ -1,9 +1,9 @@
 public extension Double {
-  func toMinutes() -> Int {
-    Int(self / 60)
-  }
+    func toMinutes() -> Int {
+        Int(self / 60)
+    }
 
-  var HHmmString: String {
-    toMinutes().HHmmString
-  }
+    var HHmmString: String {
+        toMinutes().HHmmString
+    }
 }
--- Core/Sources/Core/Int+Utils.swift
+++ Core/Sources/Core/Int+Utils.swift
@@ -1,18 +1,27 @@
 public extension Int {
-  func toSeconds() -> Double {
-    Double(self * 60)
-  }
-
-  var HHmmString: String {
-    let hours = self / 60
-    let minutes = self % 60
-
-    if minutes == 0 {
-      return "\(hours) hours"
-    } else if hours == 0 {
-      return "\(minutes) minutes"
-    } else {
-      return "\(hours)h \(minutes)m"
+    func toSeconds() -> Double {
+        Double(self * 60)
     }
-  }
+
+    var HHmmString: String {
+        let hours = self / 60
+        let minutes = self % 60
+
+        if minutes == 0 && hours == 0 {
+            return "1 minute"
+        }
+        if minutes == 0 {
+            if hours == 1 {
+                return "\(hours) hour"
+            }
+            return "\(hours) hours"
+        } else if hours == 0 {
+            if minutes == 1 {
+                return "\(minutes) minute"
+            }
+            return "\(minutes) minutes"
+        } else {
+            return "\(hours)h \(minutes)m"
+        }
+    }
 }
