21a103d0792fd05e0782a27829e0dc5f5bc7d6ff
Artem <artchzhen@gmail.com>
2023-07-24 22:51:10 +0900

    refactor PermissionView and PermissionViewController

--- Rise/Presentation/Pages/PermissionView.swift
+++ Rise/Presentation/Pages/PermissionView.swift
@@ -12,74 +12,102 @@ import Localization
 
 class PermissionView: UIView {
     lazy var titleLabel: UILabel = {
-        let label = UILabel()
-        label.font = UIFont.boldSystemFont(ofSize: 24)
-        label.textColor = .white
-        label.textAlignment = .center
-        label.translatesAutoresizingMaskIntoConstraints = false
-        return label
-    }()
-
-    lazy var messageLabel: UILabel = {
-        let label = UILabel()
-        label.font = UIFont.boldSystemFont(ofSize: 16)
-        label.textColor = .white
-        label.textAlignment = .center
-        label.numberOfLines = 0
-        label.translatesAutoresizingMaskIntoConstraints = false
-        return label
-    }()
-
-    lazy var goToSettingsButton: UIButton = {
-        let button = UIButton(type: .system)
-        button.setTitle("Go to Settings", for: .normal)
-        button.tintColor = .white
-        button.translatesAutoresizingMaskIntoConstraints = false
-        return button
-    }()
-
-    lazy var skipButton: UIButton = {
-        let button = UIButton(type: .system)
-        button.setTitle("Not Now", for: .normal)
-        button.tintColor = .white
-        button.translatesAutoresizingMaskIntoConstraints = false
-        return button
-    }()
-
-    override init(frame: CGRect) {
-        super.init(frame: frame)
-        setupView()
-    }
-
-    required init?(coder aDecoder: NSCoder) {
-        super.init(coder: aDecoder)
-        setupView()
-    }
-
-    private func setupView() {
-        backgroundColor = .clear
-
-        addSubview(titleLabel)
-        addSubview(messageLabel)
-        addSubview(goToSettingsButton)
-        addSubview(skipButton)
-
-        NSLayoutConstraint.activate([
-            titleLabel.topAnchor.constraint(equalTo: topAnchor, constant: 100),
-            titleLabel.leadingAnchor.constraint(equalTo: leadingAnchor, constant: 20),
-            titleLabel.trailingAnchor.constraint(equalTo: trailingAnchor, constant: -20),
-
-            messageLabel.topAnchor.constraint(equalTo: titleLabel.bottomAnchor, constant: 20),
-            messageLabel.leadingAnchor.constraint(equalTo: leadingAnchor, constant: 20),
-            messageLabel.trailingAnchor.constraint(equalTo: trailingAnchor, constant: -20),
-
-            goToSettingsButton.bottomAnchor.constraint(equalTo: bottomAnchor, constant: -50),
-            goToSettingsButton.leadingAnchor.constraint(equalTo: leadingAnchor, constant: 20),
-            goToSettingsButton.trailingAnchor.constraint(equalTo: trailingAnchor, constant: -20),
-
-            skipButton.bottomAnchor.constraint(equalTo: goToSettingsButton.topAnchor, constant: -10),
-            skipButton.leadingAnchor.constraint(equalTo: leadingAnchor, constant: 20),
-            skipButton.trailingAnchor.constraint(equalTo: trailingAnchor, constant: -20)
-        ])
-    }
+           let label = UILabel()
+           label.font = UIFont.systemFont(ofSize: 24, weight: .semibold)
+           label.textColor = .white
+           label.textAlignment = .center
+           label.text = "Rise"
+           label.translatesAutoresizingMaskIntoConstraints = false
+           return label
+       }()
+       
+       lazy var moonImageView: UIImageView = {
+           let imageView = UIImageView()
+           imageView.image = UIImage(systemName: "moon.zzz.fill")
+           imageView.tintColor = .white
+           imageView.contentMode = .scaleAspectFit
+           imageView.translatesAutoresizingMaskIntoConstraints = false
+           return imageView
+       }()
+       
+       lazy var messageLabel: UILabel = {
+           let label = UILabel()
+           label.font = UIFont.systemFont(ofSize: 18, weight: .medium)
+           label.textColor = .white
+           label.textAlignment = .center
+           label.numberOfLines = 0
+           label.text = """
+               Good Night! 🌙
+               We noticed that you haven't enabled notifications for Rise yet. We're here to make sure you wake up on time and start your day right!
+               For us to do that effectively, we need you to allow notifications. This ensures that we can sound your alarm even when the app isn't actively running. It's an easy one-time setup in your device's settings.
+               Will you help us, help you rise and shine every morning? ☀️
+               """
+           label.translatesAutoresizingMaskIntoConstraints = false
+           return label
+       }()
+       
+       lazy var goToSettingsButton: UIButton = {
+           let button = UIButton(type: .system)
+           button.setTitle("Go to Settings", for: .normal)
+           button.tintColor = .white
+           button.titleLabel?.font = UIFont.systemFont(ofSize: 16, weight: .semibold)
+           button.translatesAutoresizingMaskIntoConstraints = false
+           return button
+       }()
+       
+       lazy var skipButton: UIButton = {
+           let button = UIButton(type: .system)
+           button.setTitle("Not Now", for: .normal)
+           button.tintColor = .white
+           button.titleLabel?.font = UIFont.systemFont(ofSize: 16, weight: .semibold)
+           button.translatesAutoresizingMaskIntoConstraints = false
+           return button
+       }()
+       
+       override init(frame: CGRect) {
+           super.init(frame: frame)
+           setupView()
+           setupLayout()
+       }
+       
+       required init?(coder aDecoder: NSCoder) {
+           super.init(coder: aDecoder)
+           setupView()
+           setupLayout()
+       }
+       
+       private func setupView() {
+           addBackgroundView(.rich, blur: .dark)
+           backgroundColor = .clear
+           addSubview(titleLabel)
+           addSubview(moonImageView)
+           addSubview(messageLabel)
+           addSubview(goToSettingsButton)
+           addSubview(skipButton)
+       }
+       
+       private func setupLayout() {
+           NSLayoutConstraint.activate([
+               titleLabel.topAnchor.constraint(equalTo: safeAreaLayoutGuide.topAnchor, constant: 28),
+               titleLabel.leadingAnchor.constraint(equalTo: leadingAnchor, constant: 20),
+               titleLabel.trailingAnchor.constraint(equalTo: trailingAnchor, constant: -20),
+               
+               moonImageView.topAnchor.constraint(equalTo: titleLabel.bottomAnchor, constant: 28),
+               moonImageView.centerXAnchor.constraint(equalTo: centerXAnchor),
+               moonImageView.widthAnchor.constraint(equalToConstant: 80),
+               moonImageView.heightAnchor.constraint(equalToConstant: 80),
+               
+               messageLabel.topAnchor.constraint(equalTo: moonImageView.bottomAnchor, constant: 16),
+               messageLabel.leadingAnchor.constraint(equalTo: leadingAnchor, constant: 20),
+               messageLabel.trailingAnchor.constraint(equalTo: trailingAnchor, constant: -20),
+               
+               goToSettingsButton.bottomAnchor.constraint(equalTo: skipButton.topAnchor, constant: -10),
+               goToSettingsButton.leadingAnchor.constraint(equalTo: leadingAnchor, constant: 20),
+               goToSettingsButton.trailingAnchor.constraint(equalTo: trailingAnchor, constant: -20),
+               
+               skipButton.bottomAnchor.constraint(equalTo: safeAreaLayoutGuide.bottomAnchor, constant: -50),
+               skipButton.leadingAnchor.constraint(equalTo: leadingAnchor, constant: 20),
+               skipButton.trailingAnchor.constraint(equalTo: trailingAnchor, constant: -20)
+           ])
+       }
 }
--- Rise/Presentation/Pages/PermissonViewController.swift
+++ Rise/Presentation/Pages/PermissonViewController.swift
@@ -12,45 +12,28 @@ import Localization
 
 class PermissionViewController: UIViewController, ViewController {
     typealias View = PermissionView
-
-    private lazy var permissionView: PermissionView = {
-        let view = PermissionView(frame: .zero)
-        view.titleLabel.text = "Rise"
-        view.messageLabel.text = """
-               Good Night! 🌙
-               We noticed that you haven't enabled notifications for Rise yet. We're here to make sure you wake up on time and start your day right!
-               For us to do that effectively, we need you to allow notifications. This ensures that we can sound your alarm even when the app isn't actively running. It's an easy one-time setup in your device's settings.
-               Will you help us, help you rise and shine every morning? ☀️
-               """
-        view.goToSettingsButton.addTarget(self, action: #selector(goToSettingsTapped), for: .touchUpInside)
-        view.skipButton.addTarget(self, action: #selector(skipButtonTapped), for: .touchUpInside)
-        view.translatesAutoresizingMaskIntoConstraints = false
-        return view
-    }()
+    
+    var goToSettingsAction: (() -> Void)?
+    var skipAction: (() -> Void)?
+    
+       override func loadView() {
+           super.loadView()
+           self.view = PermissionView()
+       }
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        setupView()
+        if let permissionView = view as? PermissionView {
+            permissionView.goToSettingsButton.addTarget(self, action: #selector(goToSettingsTapped), for: .touchUpInside)
+            permissionView.skipButton.addTarget(self, action: #selector(skipButtonTapped), for: .touchUpInside)
+        }
     }
 
-    private func setupView() {
-        view.addBackgroundView (.rich, blur: .dark)
-        view.backgroundColor = .clear
-        view.addSubview(permissionView)
+       @objc  func goToSettingsTapped() {
+           goToSettingsAction?()
+       }
 
-        NSLayoutConstraint.activate([
-            permissionView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor),
-            permissionView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
-            permissionView.trailingAnchor.constraint(equalTo: view.trailingAnchor),
-            permissionView.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor)
-        ])
-    }
-
-    @objc private func goToSettingsTapped() {
-        UIApplication.openAppSettings()
-    }
-
-    @objc private func skipButtonTapped() {
-        dismiss(animated: true)
-    }
+       @objc  func skipButtonTapped() {
+           skipAction?()
+       }
 }
